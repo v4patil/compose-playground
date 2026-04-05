59d6b6c8489a24c4de4bcf62f5d52b2dde5adaa9
V4_patil <vibhorbPatil@gmail.com>
2026-04-05 20:54:18 +0530

    Setup image read button operation and set the image on the imageView

--- app/src/main/java/com/vibhorpatil/composeapplication/paintapp/data/PaintActivity.kt
+++ app/src/main/java/com/vibhorpatil/composeapplication/paintapp/data/PaintActivity.kt
@@ -1,12 +1,19 @@
 package com.vibhorpatil.composeapplication.paintapp.data
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import com.vibhorpatil.composeapplication.R
@@ -16,7 +23,11 @@ class PaintActivity : ComponentActivity() {
     private lateinit var drawingView: DrawingView
     private lateinit var ibBrushSize: ImageButton
     private lateinit var llColorChooser: LinearLayout
+    private lateinit var ibChooseImage: ImageButton
+    private lateinit var ivBackground: ImageView
     private lateinit var mImageButtonCurrentPaint: ImageButton
+    private lateinit var requestPermission: ActivityResultLauncher<Array<String>>
+    private lateinit var openGalleryLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,12 +41,48 @@ class PaintActivity : ComponentActivity() {
         mImageButtonCurrentPaint.setImageDrawable(
             ContextCompat.getDrawable(this, R.drawable.pallet_selected)
         )
+        setClickListeners()
+        registerLauncher()
     }
 
     private fun generateId() {
         drawingView = findViewById(R.id.drawingView)
         ibBrushSize = findViewById(R.id.ibBrushSize)
         llColorChooser = findViewById(R.id.llColorChooser)
+        ibChooseImage = findViewById(R.id.ibChooseImage)
+        ivBackground = findViewById(R.id.ivBackground)
+    }
+
+    private fun setClickListeners() {
+        ibChooseImage.setOnClickListener {
+            requestStoragePermission()
+        }
+    }
+
+    private fun registerLauncher() {
+        requestPermission =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                permissions.entries.forEach {
+                    val permissionName = it.key
+                    val isGranted = it.value
+                    if (isGranted) {
+                        val pickIntent =
+                            Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                        openGalleryLauncher.launch(pickIntent)
+                    } else {
+                        if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE) {
+
+                        }
+                    }
+                }
+            }
+
+        openGalleryLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK && result.data != null) {
+                    ivBackground.setImageURI(result.data?.data)
+                }
+            }
     }
 
     private fun showBrushSizeChooserDialog() {
@@ -62,7 +109,7 @@ class PaintActivity : ComponentActivity() {
         if (view != mImageButtonCurrentPaint) {
             val imageButton = view as ImageButton
             val colorTag = imageButton.tag.toString()
-            drawingView?.setColor(colorTag)
+            drawingView.setColor(colorTag)
             imageButton.setImageDrawable(
                 ContextCompat.getDrawable(this, R.drawable.pallet_selected)
             )
@@ -72,6 +119,32 @@ class PaintActivity : ComponentActivity() {
             )
 
             mImageButtonCurrentPaint = imageButton
+        }
+    }
+
+    fun showRationalDialog(title: String, message: String) {
+        val rationalDialogBuilder = AlertDialog.Builder(this)
+
+        rationalDialogBuilder.setTitle(title)
+            .setMessage(message)
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+            .show()
+    }
+
+    private fun requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        ) {
+            showRationalDialog("Drawing App", "Need permission to load image from gallery")
+        } else {
+            requestPermission.launch(
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            )
         }
     }
 
